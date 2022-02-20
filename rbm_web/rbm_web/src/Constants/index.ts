fc446bea46b0fcf9f7c0e6118bde0867567a57f6
zhaojianhua <1161223922@qq.com>
2022-02-20 13:19:14 +0800

    新增常量和方法文件

---
+++ rbm_web/rbm_web/src/Constants/index.ts
@@ -0,0 +1,5 @@
+const Constants:Object = {
+
+}
+
+export default Constants
---
+++ rbm_web/rbm_web/src/Utils/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: mujin
+ * @Date: 2022-02-20 13:01:59
+ * @LastEditTime: 2022-02-20 13:14:52
+ * @Description: 
+ */
+
+const Utils:Object = {
+  
+}
+
+export default Utils;
