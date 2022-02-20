761ebbe3446bc666844ed96d370bf05f1cd88639
zhaojianhua <1161223922@qq.com>
2022-02-20 17:33:58 +0800

    【新增】：初始化项目

---
+++ rbm/src/test/java/com/blog/rbm/RbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.rbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
